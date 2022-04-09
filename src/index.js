dffed013afc9048928a596d2bfe64789e69ce6d9
Vlad Ostapuk <Sp0ngbik@gmail.com>
2022-04-09 22:22:56 +0300

    redux input with value works

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import mainReducer from "./pages/ReduxPractice/reducer/mainReducer";
+
+const store = createStore(
+  mainReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/ReduxPractice/redux/ReduxIndex.js
+++ src/pages/ReduxPractice/redux/ReduxIndex.js
@@ -1,9 +1,28 @@
 import { Component } from "react";
+import { connect } from "react-redux";
 
 class ReduxIndex extends Component {
+  addElement() {
+    this.props.onAddElement(this.addElementInput.value);
+    this.addElementInput.value = "";
+  }
   render() {
-    return <div>s</div>;
+    return (
+      <div>
+        <input type="text" ref={(input) => (this.addElementInput = input)} />
+        <button onClick={this.addElement.bind(this)}>Добавить элемент</button>
+      </div>
+    );
   }
 }
 
-export default ReduxIndex;
+export default connect(
+  (state) => ({
+    state,
+  }),
+  (dispatch) => ({
+    onAddElement: (inputValue) => {
+      dispatch({ type: "ADD_ELEMENT", payload: inputValue });
+    },
+  })
+)(ReduxIndex);
