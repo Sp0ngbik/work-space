67bb1504cea7127ccd8b1688268bc57ffa4b2f1c
Vlad Ostapuk <Sp0ngbik@gmail.com>
2022-04-26 21:01:33 +0300

    miss block

--- src/pages/PartfolioPages/FirstSite/FirstSite.js
+++ src/pages/PartfolioPages/FirstSite/FirstSite.js
@@ -326,6 +326,29 @@ const FirstSite = () => {
             </div>
           </div>
         </section>
+        <section className={style.nonMissMessage}>
+          <div className={style.nonMissBlock}>
+            <div className={style.leftMissBlock}>
+              <h5>Don't miss out!</h5>
+              <div>
+                Sign-up & we'll let you know as soon as we have something to
+                show off. (Don't worry, we don't spam).
+              </div>
+            </div>
+            <div className={style.rightMissBlock}>
+              <div>
+                <input placeholder="Enter your email" />
+                <button>Sign up</button>
+              </div>
+              <div className={style.rightMissDiscr}>
+                <div>
+                  By clicking Sign Up you're confirming that you agree with our
+                  <span> Terms and Conditions .</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
